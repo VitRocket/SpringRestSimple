8f7aacc52c95de155a0726e18dc663b4450d0fa7
VitRocket <vitrocket@gmail.com>
2018-05-24 13:05:05 +0300

    Start REST. Add Maven plugin. Test http://localhost:8080/test

---
+++ src/main/java/vitrocket/spring/server/config/DispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package vitrocket.spring.server.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
